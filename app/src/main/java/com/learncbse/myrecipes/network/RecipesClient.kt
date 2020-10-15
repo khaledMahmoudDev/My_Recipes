b7367af5946ace36000270b7a13fd70426bf8b81
KhalidMahmoudDev <khaled.0155067@ci.suez.edu.eg>
2020-10-15 14:14:51 +0200

    build network client

--- app/src/main/java/com/learncbse/myrecipes/network/RecipesClient.kt
+++ app/src/main/java/com/learncbse/myrecipes/network/RecipesClient.kt
@@ -1,4 +1,25 @@
 package com.learncbse.myrecipes.network
 
-class RecipesClient {
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.learncbse.myrecipes.utils.RECIPES_BASE_URL
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+
+val retrofit: Retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(RECIPES_BASE_URL)
+    .build()
+
+object RecipesClient {
+    val allRecipes: RecipeApi by lazy {
+        retrofit.create(RecipeApi::class.java)
+    }
 }
+
